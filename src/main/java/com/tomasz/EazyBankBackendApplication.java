e80bab0b01729d7de6592cde0fcc302637d51c9c
tomszociepa <tom.ociepa@gmail.com>
2023-08-16 14:42:12 +0200

    basic url access configuration

--- src/main/java/com/tomasz/EazyBankBackendApplication.java
+++ src/main/java/com/tomasz/EazyBankBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("package com.tomasz.controller") // optional. Spring zawsze skanuje cały pakiet w @SpringBootApplication
+//@ComponentScan("package com.tomasz.controller") // optional. Spring zawsze skanuje cały pakiet w @SpringBootApplication
 public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
