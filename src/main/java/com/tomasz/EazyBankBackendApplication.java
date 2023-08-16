c7e9c859bcea5acc2fd14332a8a6b8e85d6afd17
tomszociepa <tom.ociepa@gmail.com>
2023-08-16 13:22:29 +0200

    init app

---
+++ src/main/java/com/tomasz/EazyBankBackendApplication.java
@@ -0,0 +1,16 @@
+package com.tomasz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("package com.tomasz.controller") // optional. Spring zawsze skanuje cały pakiet w @SpringBootApplication
+public class EazyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankBackendApplication.class, args);
+	}
+
+}
+ 
