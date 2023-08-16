e5a32ae3fbc92f451ec8a9e55fd9f566e98257de
tomszociepa <tom.ociepa@gmail.com>
2023-08-16 13:57:32 +0200

    create controllers

---
+++ src/main/java/com/tomasz/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.tomasz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/myAccount")
+    public String getAccountDetails(){
+        return "Here are account details from DB";
+    }
+}
---
+++ src/main/java/com/tomasz/controller/LoansController.java
@@ -0,0 +1,13 @@
+package com.tomasz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoansController {
+
+    @GetMapping("/myLoans")
+    public String getLoansDetails(){
+        return "There are loan details from DB";
+    }
+}
